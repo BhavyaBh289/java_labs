9a172f8d165f9dd12622ade25e680482251dde4e
BhavyaBh289 <shahbhavyasp@gmail.com>
2023-02-22 12:57:51 +0530

    duplicate strings

--- dup_num.java
+++ dup_num.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 
 class find_dupes {
     public static void main (String[] args){
-    Scanner s = System.in()
+    Scanner s = new Scanner(System.in);
+    System.out.println("entre the string");
+    String  text= s.next();
+    System.out.println("entre the substring");
+    String  subtext= s.next();
+    System.out.println(text);
 	}
 }
 
