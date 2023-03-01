0de85558b6cf957fd76a4b49df2bcb1f9cb79bd1
BhavyaBh289 <shahbhavyasp@gmail.com>
2023-03-01 12:36:09 +0530

    duplicate strings

--- dup_num.java
+++ dup_num.java
@@ -4,10 +4,11 @@ class find_dupes {
     public static void main (String[] args){
     Scanner s = new Scanner(System.in);
     System.out.println("entre the string");
-    String  text= s.next();
+    String  text= s.nextLine();
     System.out.println("entre the substring");
-    String  subtext= s.next();
-    System.out.println(text);
-	}
+    String  subtext= s.nextLine();
+    System.out.println(text.contains(subtext));
+
+    }
 }
 
---
+++ name_handling.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class name_handle {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("entre the name");
+        String text = s.nextLine();
+        System.out.println("hello "+s);
+
+    }
+}
