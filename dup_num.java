49248b6e1b25bd646d34c715dc9729cc5cf0d399
BhavyaBh289 <shahbhavyasp@gmail.com>
2023-04-02 23:31:31 +0530

    1

--- dup_num.java
+++ dup_num.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+// exp 2,3 strings
 class find_dupes {
     public static void main (String[] args){
     Scanner s = new Scanner(System.in);
--- name_handling.java
+++ name_handling.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
-
+//exp 4 strjhkk
 class name_handle {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("entre the name");
         String text = s.nextLine();
-        System.out.println("hello "+s);
+        System.out.println("hello "+text.charAt(0) +" "+text.charAt(text.indexOf(" ")+1) + text.substring(text.substring(text.indexOf(" ")+1).indexOf(" ")+text.indexOf(" ")+1) );
+        System.out.println();
 
     }
 }
